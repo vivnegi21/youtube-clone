7435b82c5600085a66d47b0c4b6b158784bd38ba
vivnegi21 <vivnegi001@gmail.com>
2023-03-02 18:29:29 +0530

    Init COmmit

---
+++ src/components/Feed.jsx
@@ -0,0 +1,10 @@
+import {useState, useEffect} from 'react';
+import { Box,Stack, typography } from '@mui/system';
+
+const Feed = () => {
+  return (
+    <div>Feed</div>
+  )
+}
+
+export default Feed
