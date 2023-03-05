2d116fa640e123b6bd88cdafb0d0050248a19262
vivnegi21 <vivnegi001@gmail.com>
2023-03-05 16:53:57 +0530

    changed the issue with video playing problem

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -8,13 +8,13 @@ import { demoThumbnailUrl, demoVideoUrl,demoChannelUrl,demoChannelTitle,demoVide
 
 const VideoCard = ({ video: { id: { videoId }, snippet } }) => (
   <Card sx={{ width: { xs: '100%', sm: '358px', md: "320px", }, boxShadow: "none", borderRadius: 0 }}>
-    <Link to={videoId ? `/video/${videoId}` : `/video/cV2gBU6hKfY` }>
+    <Link to={videoId ? `/youtube-clone/video/${videoId}` : `/youtube-clone/video/cV2gBU6hKfY` }>
       <CardMedia image={snippet?.thumbnails?.high?.url || demoThumbnailUrl} alt={snippet?.title} 
         sx={{ width: { xs: '100%', sm: '358px'}, height: 180 }} 
       />
     </Link>
     <CardContent sx={{ backgroundColor: "#1E1E1E", height: '106px' }}>
-      <Link to={videoId ? `/video/${videoId}` : demoVideoUrl } >
+      <Link to={videoId ? `/youtube-clone/video/${videoId}` : demoVideoUrl } >
         <Typography variant="subtitle1" fontWeight="bold" color="#FFF">
           {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
         </Typography>
