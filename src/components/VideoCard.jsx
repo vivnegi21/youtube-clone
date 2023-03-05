fe4b13f99327ca4ea636e5599985a9b8ed135cff
vivnegi21 <vivnegi001@gmail.com>
2023-03-05 17:41:03 +0530

    first deploy to revisit to if wrong

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -8,18 +8,18 @@ import { demoThumbnailUrl, demoVideoUrl,demoChannelUrl,demoChannelTitle,demoVide
 
 const VideoCard = ({ video: { id: { videoId }, snippet } }) => (
   <Card sx={{ width: { xs: '100%', sm: '358px', md: "320px", }, boxShadow: "none", borderRadius: 0 }}>
-    <Link to={videoId ? `/youtube-clone/video/${videoId}` : `/youtube-clone/video/cV2gBU6hKfY` }>
+    <Link to={videoId ? `/video/${videoId}` : `/video/cV2gBU6hKfY` }>
       <CardMedia image={snippet?.thumbnails?.high?.url || demoThumbnailUrl} alt={snippet?.title} 
         sx={{ width: { xs: '100%', sm: '358px'}, height: 180 }} 
       />
     </Link>
     <CardContent sx={{ backgroundColor: "#1E1E1E", height: '106px' }}>
-      <Link to={videoId ? `/youtube-clone/video/${videoId}` : demoVideoUrl } >
+      <Link to={videoId ? `/video/${videoId}` : demoVideoUrl } >
         <Typography variant="subtitle1" fontWeight="bold" color="#FFF">
           {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
         </Typography>
       </Link>
-      <Link to={snippet?.channelId ? `/youtube-clone/channel/${snippet?.channelId}` : demoChannelUrl} >
+      <Link to={snippet?.channelId ? `/channel/${snippet?.channelId}` : demoChannelUrl} >
         <Typography variant="subtitle2" color="gray">
           {snippet?.channelTitle || demoChannelTitle}
           <CheckCircle sx={{ fontSize: "12px", color: "gray", ml: "5px" }} />
